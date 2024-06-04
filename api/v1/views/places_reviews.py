173a6336edceb4c1cda0c477cc50d6691b4bab6b
Wayline Jeffries <jeffrieswayline@gmail.com>
2024-06-04 10:35:55 +0200

    reviews update

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,6 +4,8 @@ from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
 from models.place import Place
+from models.review import Review
+from models.user import User
 
 @app_views.route('/places/<place_id>/reviews',
                  methods=['GET'], strict_slashes=False)
