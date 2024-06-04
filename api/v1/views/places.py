7dc1aacafc779f17b9b8c98744ef616cc25ca78a
Wayline Jeffries <jeffrieswayline@gmail.com>
2024-06-04 10:41:59 +0200

    updates

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,8 +4,8 @@ from flask import abort, jsonify, make_response, request
 import requests
 from api.v1.views import app_views
 from api.v1.views.amenities import amenities
-
 from models import storage
+from api.v1.views.places_amenities import place_amenities
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
